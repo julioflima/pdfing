dbd1770972955c403780b4fd155142f091ce13c3
juloko <julio_flima@hotmail.com>
2020-11-16 14:49:49 -0300

    Add more classes.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,3 @@
-const Pdf = require('./class/Pdf');
-const Process = require('./class/Process');
+const Pdfing = require('./class/Pdfing');
 
-module.exports = class Pdfing {
-  constructor(options, data) {
-    this.options = options;
-    this.data = data;
-
-    this.Pdf = Pdf;
-    this.Process = Process;
-
-    return this.init();
-  }
-
-  init() {
-    const processedData = new this.Process(this.data);
-    const pdf = new this.Pdf(this.options, processedData);
-
-    return pdf;
-  }
-};
+module.exports = Pdfing;
